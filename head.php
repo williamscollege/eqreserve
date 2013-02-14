8a1020cc77f80cacecad487e2fad922f5df25f1b
Chris Warren <cwarren@williams.edu>
2013-02-14 15:45:02 -0500

    move login-form and logged-in-controls to the head.php file; in the session, create an array to hold all the user data (makes unsetting all of it at once on logout much easier - also more flexible to hold additional user data later)

--- head.php
+++ head.php
@@ -2,26 +2,26 @@
 session_start();
 require_once('institution.cfg.php');
 
-echo "head loaded\n";
-
 if ((! isset($_SESSION['isAuthenticated'])) || (! $_SESSION['isAuthenticated'])) {
-//echo 'session not authenticated';
     if ((isset($_REQUEST['username'])) && (isset($_REQUEST['password']))) {
-//echo "trying to authenticate\n";
-//echo 'username = '.$_REQUEST['username']."\n";
-//echo 'password = '.$_REQUEST['password']."\n";
         require_once('auth.cfg.php');
         if ($AUTH->authenticate($_REQUEST['username'],$_REQUEST['password'])) {
             session_regenerate_id(TRUE);
             $_SESSION['isAuthenticated'] = true;
-            $_SESSION['username'] = $_REQUEST['username'];
+            $_SESSION['userdata'] = array();
+            $_SESSION['userdata']['username'] = $_REQUEST['username'];
         }
-//echo "post authenticate attempt\n";
-//echo 'isAuthenticated = '.$_SESSION['isAuthenticated']."\n";
-    }    
+    }
 }
-
-$DB = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME.";port=3306",DB_USER,DB_PASS);
+else {
+    if (isset($_REQUEST['logout'])) {
+        unset($_SESSION['isAuthenticated']);
+        unset($_SESSION['userdata']);
+    }
+    else {
+        $DB = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME.";port=3306",DB_USER,DB_PASS);
+    }
+}
 
 ?>
 <html>
@@ -29,3 +29,21 @@ $DB = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME.";port=3306",DB_USER,DB
   <title><?php echo APP_NAME.': '.$pageTitle;?></title>
  </head>
  <body>
+<?php 
+if ((isset($_SESSION['isAuthenticated'])) && ($_SESSION['isAuthenticated'])) {
+?>
+<div id="loggedInControls">You are logged in as <?php echo $_SESSION['userdata']['username']; ?>. <form id="frmLogout" class="" type="post" action=""><input type="submit" name="logout" id="logout_btn" value="log out"/></form></div>
+<?php
+} 
+else if ((! isset($_SESSION['isAuthenticated'])) || (! $_SESSION['isAuthenticated'])) {
+?>
+<form id="frmIndex" class="" type="post" action="">
+    <input type="text" id="username" name="username" value="" />
+    <input type="password" id="password" name="password" value="" />
+    <input type="submit" id="submit_login" name="submit_login" value="log In" />
+
+</form>
+<?php
+    require_once('foot.php');
+    exit;
+}
--- index.php
+++ index.php
@@ -2,23 +2,5 @@
 $pageTitle = 'Home';
 require_once('head.php');
 
-if ((! isset($_SESSION['isAuthenticated'])) || (! $_SESSION['isAuthenticated'])) {
-?>
-<form id="frmIndex" class="" type="post" action="">
-	<input type="text" id="username" name="username" value="" />
-	<input type="password" id="password" name="password" value="" />
-	<input type="submit" id="submit_login" name="submit_login" value="Log In" />
-
-</form>
-<?php
-} else
-{
-?>
-
-You are logged in as <?php echo $_SESSION['username']; ?>. <input type="button" name="logout" id="logout_btn" value="log out"/>
-
-<?php
-$_SESSION['isAuthenticated'] = false;
-}
 require_once('foot.php');
 ?>
