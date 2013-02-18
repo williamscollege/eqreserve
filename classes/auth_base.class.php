560bde7fb7d752f25f649b322510a027fbbd610a
dwk2 <dwk2@williams.edu>
2013-02-18 11:43:41 -0500

    Completed LDAP authentication subclass. Added session data as visible output (for testing purposes) to authenticated head.php page

--- classes/auth_base.class.php
+++ classes/auth_base.class.php
@@ -3,15 +3,25 @@ class Auth_Base
 {
 
 	# define attributes of object
-	public $err_msg;
-	public $err_code;
-	public $err_severity;
-	
+	public $auth_msg;
+	public $auth_position;
+	public $auth_mail;
+	public $auth_lname;
+	public $auth_fname;
+	public $auth_name;
+	public $auth_sortname;
+	public $auth_debug;
+
 	
     public function authenticate($user,$pass) {
-		$this->err_msg = '';
-		$this->err_code = '';
-		$this->err_severity = '';
+		$this->auth_msg			= '';
+		$this->auth_position	= '';
+		$this->auth_mail		= '';
+		$this->auth_lname		= '';
+		$this->auth_fname		= '';
+		$this->auth_name		= '';
+		$this->auth_sortname	= '';
+		$this->auth_debug		= '';
 
 		//echo "authenticating...\n";
 		//echo 'user='.$user."\n";
--- head.php
+++ head.php
@@ -3,23 +3,30 @@ session_start();
 require_once('institution.cfg.php');
 
 if ((! isset($_SESSION['isAuthenticated'])) || (! $_SESSION['isAuthenticated'])) {
-
     if ((isset($_REQUEST['username'])) && (isset($_REQUEST['password']))) {
-
         require_once('auth.cfg.php');
 
         if ($AUTH->authenticate($_REQUEST['username'],$_REQUEST['password'])) {
             session_regenerate_id(TRUE);
             $_SESSION['isAuthenticated'] = true;
             $_SESSION['userdata'] = array();
-            $_SESSION['userdata']['username'] = $_REQUEST['username'];
+            $_SESSION['userdata']['username']	= $_REQUEST['username'];
+            $_SESSION['userdata']['email']		= $AUTH->auth_mail;
+            $_SESSION['userdata']['fullname']	= $AUTH->auth_name;
+            $_SESSION['userdata']['firstname']	= $AUTH->auth_fname;
+            $_SESSION['userdata']['lastname']	= $AUTH->auth_lname;
+            $_SESSION['userdata']['sortname']	= $AUTH->auth_sortname;
+            $_SESSION['userdata']['position']	= $AUTH->auth_position; // e.g. (STUDENT, FACULTY, STAFF)
         }
+        
 		# START: Debugging Info
-		echo '<br />isAuthenticated = '.$_SESSION['isAuthenticated']."<br />\n";
-		if ($AUTH->err_msg != '') {
-			// echo the attribute of the $AUTH object
-			echo "Errors found: " . $AUTH->err_msg . "<br />";
-		}
+			echo "<br /><h2>Development Messages</h2>";
+			# echo "isAuthenticated = ".$_SESSION['isAuthenticated']."<br />\n";
+			#if ($AUTH->auth_msg != '' || $AUTH->auth_debug != '') {
+				// echo the attribute of the $AUTH object
+				echo "<b>Message found:</b><br />" . $AUTH->auth_msg . "<br />";
+				echo "<b>Debug Info:</b><br />" . $AUTH->auth_debug . "<br />";
+			#}
 		# END: Debugging Info
     }
 }
@@ -42,7 +49,18 @@ else {
 <?php 
 if ((isset($_SESSION['isAuthenticated'])) && ($_SESSION['isAuthenticated'])) {
 ?>
-<div id="loggedInControls">You are logged in as <?php echo $_SESSION['userdata']['username']; ?>. <form id="frmLogout" class="" type="post" action=""><input type="submit" name="logout" id="logout_btn" value="log out"/></form></div>
+<div id="loggedInControls">
+	You are logged in as:<br />
+		<?php 
+			echo "username: " . $_SESSION['userdata']['username'] . "<br />";
+			echo "email: " . $_SESSION['userdata']['email'] . "<br />";
+			echo "fullname: " . $_SESSION['userdata']['fullname'] . "<br />";
+			echo "firstname: " . $_SESSION['userdata']['firstname'] . "<br />";
+			echo "lastname: " . $_SESSION['userdata']['lastname'] . "<br />";
+			echo "sortname: " . $_SESSION['userdata']['sortname'] . "<br />";
+			echo "position: " . $_SESSION['userdata']['position'] . "<br />";
+		?>
+		<form id="frmLogout" class="" type="post" action=""><input type="submit" name="logout" id="logout_btn" value="log out"/></form></div>
 <?php
 } 
 else if ((! isset($_SESSION['isAuthenticated'])) || (! $_SESSION['isAuthenticated'])) {
@@ -51,7 +69,6 @@ else if ((! isset($_SESSION['isAuthenticated'])) || (! $_SESSION['isAuthenticate
     <input type="text" id="username" name="username" value="" />
     <input type="password" id="password" name="password" value="" />
     <input type="submit" id="submit_login" name="submit_login" value="log In" />
-
 </form>
 <?php
     require_once('foot.php');
