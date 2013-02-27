a79a62d8c719ee92da7e127a3e1a1d5ebcf1e5b7
Chris Warren <cwarren@williams.edu>
2013-02-27 14:41:03 -0500

    add user method to load inst groups for that user (from the eq reserve DB, not the auth source); instantiate user in head after auth; minclean up unused code from db_linked class

--- classes/user.class.php
+++ classes/user.class.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__FILE__) . '/db_linked.class.php';
+require_once dirname(__FILE__) . '/inst_group.class.php';
 
 class User extends Db_Linked
 {
@@ -7,13 +8,40 @@ class User extends Db_Linked
     public static $primaryKeyField = 'user_id';    
     public static $dbTable = 'users';
 
+    public $inst_groups;
+
     public function __construct($initsHash) {
 		parent::__construct($initsHash);
-		
+	
 		// now do custom stuff
-		// e.g. automatically load all accesibility info assocaited with the user
-	}
+		// e.g. automatically load all accesibility info associated with the user
 
+        if ($this->user_id) {
+            $this->loadInstGroups();
+        }
+    }
+
+    public function loadInstGroups() {
+        if (! $this->user_id) {
+            trigger_error('cannot load inst groups for a user with no user_id');
+            return;
+        }
+        $this->inst_groups = [];
+        $getInstGroupsSql = "SELECT ig.inst_group_id, ig.name, ig.flag_delete 
+                             FROM ".InstGroup::$dbTable." AS ig, link_users_inst_groups AS link 
+                             WHERE link.user_id = ".$this->user_id." AND ig.inst_group_id = link.inst_group_id
+                                AND link.flag_delete = 0 AND ig.flag_delete = 0";
+        $getInstGroupsStmt = $this->dbConnection->prepare($getInstGroupsSql);
+        $getInstGroupsStmt->execute();
+        while ($row = $getInstGroupsStmt->fetch()) {
+            $ig = new InstGroup(['DB'=>$this->dbConnection,'inst_group_id'=>$row['inst_group_id'],'name'=>$row['name'],'flag_delete'=>0]);
+            array_push($this->inst_groups,$ig);
+        }
+    }
+
+//    public function loadEgGroups() {
+//        $this->eq_groups = EqGroups::loadEqGroupsForUser($this);
+//    }
 
 	public function updateDbFromAuth($auth) {
 /*
