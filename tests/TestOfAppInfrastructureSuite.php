e4be59e71f43db2181be5f2eae334a6fc1e765ff
Chris Warren <cwarren@williams.edu>
2013-03-01 10:29:02 -0500

    inst group can link and unlink a user; inst group can give a list of all users in the group

--- tests/TestOfAppInfrastructureSuite.php
+++ tests/TestOfAppInfrastructureSuite.php
@@ -10,8 +10,9 @@ class TestOfAppInfrastructureSuite extends TestSuite {
         $this->addFile('app_infrastructure_tests/TestOfDB_Linked.class.php');
         $this->addFile('app_infrastructure_tests/TestOfAuth_Base.class.php');
         $this->addFile('app_infrastructure_tests/TestOfAuth_LDAP.class.php');
+        $this->addFile('app_infrastructure_tests/TestOfInstGroup.class.php');
         $this->addFile('app_infrastructure_tests/TestOfUser.class.php');
-        $this->addFile('app_infrastructure_tests/TestOfEqGroup.class.php');
+        // $this->addFile('app_infrastructure_tests/TestOfEqGroup.class.php');
      }
 }
 ?>
--- tests/app_infrastructure_tests/TestOfInstGroup.class.php
+++ tests/app_infrastructure_tests/TestOfInstGroup.class.php
@@ -47,7 +47,69 @@ class TestOfInstGroup extends UnitTestCaseDB {
         $this->assertTrue(in_array('flag_delete',InstGroup::$fields));      
 	}
 
-    // DB interaction tests
+
+    // DB interaction tests - object instance tests
+
+    function testLinkUser() {
+        $u = User::loadOneFromDb(['user_id'=>1],$this->DB);
+        $u->loadInstGroups();
+        $this->assertEqual(count($u->inst_groups),1);
+
+        $g = InstGroup::loadOneFromDb(['inst_group_id'=>4],$this->DB);
+        $this->assertNotEqual($u->inst_groups[0]->inst_group_id,$g->inst_gorup_id);
+
+
+        $g->linkUser($u);
+        //$u->loadInstGroups();
+
+
+        $this->assertEqual(count($u->inst_groups),2);
+
+        $new_g = $u->inst_groups[0];
+        if ($new_g->inst_group_id != 4) {
+            $new_g = $u->inst_groups[1];
+        }
+        $this->assertEqual($new_g->inst_group_id,4);
+    } 
+
+    function testUnlinkUser() {
+        // remove the connection between this group and a given user
+        $u = User::loadOneFromDb(['user_id'=>1],$this->DB);
+        $u->loadInstGroups();
+        $this->assertEqual(count($u->inst_groups),1);
+
+        $u->inst_groups[0]->unlinkUser($u);
+
+        $this->assertEqual(count($u->inst_groups),0);
+    } 
+
+    function testGetUsers() {
+        // get a list of all the user objects associated with this group
+        $g = InstGroup::loadOneFromDb(['inst_group_id'=>1],$this->DB);
+
+        $users = $g->getAllUsers();
+
+        $this->assertEqual(count($users),1);
+        $this->assertEqual($users[0]->user_id,1);
+
+        ///////
+
+        $g = InstGroup::loadOneFromDb(['inst_group_id'=>2],$this->DB);
+
+        $users = $g->getAllUsers();
+
+        $this->assertEqual(count($users),0);
+
+        ///////
+
+        $g = InstGroup::loadOneFromDb(['inst_group_id'=>4],$this->DB);
+
+        $users = $g->getAllUsers();
+
+        $this->assertEqual(count($users),0);
+    } 
+
+    // DB interaction tests - static method tests
 
     function testGetUserInstGroups() {
         $u = User::loadOneFromDb(['user_id'=>1],$this->DB);
@@ -58,7 +120,7 @@ class TestOfInstGroup extends UnitTestCaseDB {
         $this->assertEqual(count($groups),1);        
         $this->assertEqual(get_class($groups[0]),'InstGroup');
         $this->assertEqual($groups[0]->name,Auth_Base::$TEST_INST_GROUPS[0]);
+    }
 
-    }   
 }
 ?>
