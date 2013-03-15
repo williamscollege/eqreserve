11e3a279a71b10e93cdda9b81347a6df20007167
Chris Warren <cwarren@williams.edu>
2013-03-15 16:25:38 -0400

    test of subgroup uses centralized test data

--- tests/app_infrastructure_tests/TestOfEqSubgroup.class.php
+++ tests/app_infrastructure_tests/TestOfEqSubgroup.class.php
@@ -1,48 +1,19 @@
 <?php
 	require_once dirname(__FILE__) . '/../simpletest/unit_tester_DB.php';
-	require_once dirname(__FILE__) . '/../../classes/eq_subgroup.class.php';
+    require_once dirname(__FILE__) . '/dataForTesting.php';
 
 
 	class TestOfEqSubgroup extends UnitTestCaseDB
 	{
 
 		function setUp() {
-            /*
-                eq group Nanomajigs has 2 subgroups, sgA and sgB
-                eq group 3D Printers (deleted) has 1 subgroups, sgC
-                eq group Spectrometers has 2 subgroups, sgD and sgE (deleted)
-            */
-
-            # EqGroup: eq_group_id, name, descr, start_minute, min_duration_minutes, max_duration_minutes, duration_chunk_minutes, flag_delete
-            $addTestEqGroupsSql  = "INSERT INTO " . EqGroup::$dbTable . " VALUES    (1,'Nanomajigs','The investigation of really small stuff','0,15,30,45',15,60,15,0),
-                                                                                    (2,'3D Printers','3dp descr','0,30',30,300,30,1),
-                                                                                    (3,'Spectrometers','spectrothingies','0,15,30,45',15,60,15,0)
-                                                                                ";
-            $addTestEqGroupsStmt = $this->DB->prepare($addTestEqGroupsSql);
-            $addTestEqGroupsStmt->execute();
-
-
-            # EqSubgroup: eq_subgroup_id', 'eq_group_id', 'name','descr','ordering','flag_delete'
-            $addTestEqSubgroupsSql  = "INSERT INTO " . EqSubgroup::$dbTable . " VALUES 
-                                                                                    (1,1,'sgA','sg A',1,0),
-                                                                                    (2,1,'sgB','sg B',1,0),
-                                                                                    (3,2,'sgC','sg C',2,0),
-                                                                                    (4,3,'sgD','sg D',3,0),
-                                                                                    (5,3,'sgE','sg E',4,1)
-                                                                                ";
-            $addTestEqSubgroupsStmt = $this->DB->prepare($addTestEqSubgroupsSql);
-            $addTestEqSubgroupsStmt->execute();
+            createTestData_EqGroups($this->DB);
+            createTestData_EqSubgroups($this->DB);
 		}
 
 		function tearDown() {
-            $rmTestEqGroupsSql = "DELETE FROM ".EqGroup::$dbTable;
-            $rmTestEqGroupsStmt = $this->DB->prepare($rmTestEqGroupsSql);
-            $rmTestEqGroupsStmt->execute();
-
-            $rmTestEqSubgroupsSql = "DELETE FROM ".EqSubgroup::$dbTable;
-            $rmTestEqSubgroupsStmt = $this->DB->prepare($rmTestEqSubgroupsSql);
-            $rmTestEqSubgroupsStmt->execute();
-
+            removeTestData_EqGroups($this->DB);
+            removeTestData_EqSubgroups($this->DB);
 		}
 
 		//###################################
@@ -94,8 +65,8 @@
         // instance tests
         
         public function TestOfLoadEgGroup() {
-            $esg = EqSubgroup::getOneFromDb(['eq_subgroup_id'=>1],$this->DB);
-            $this->assertEqual($esg->name,'sgA');
+            $esg = EqSubgroup::getOneFromDb(['eq_subgroup_id'=>301],$this->DB);
+            $this->assertEqual($esg->name,'testSubgroup1');
             $this->assertNull($esg->eq_group);
 
             // testing this
@@ -103,7 +74,7 @@
 
             $this->assertTrue(is_object($esg->eq_group));
             $this->assertEqual(get_class($esg->eq_group),'EqGroup');
-            $this->assertEqual($esg->eq_group->name,'Nanomajigs');
+            $this->assertEqual($esg->eq_group->name,'testEqGroup1');
         }
 
 	}
