18d9bd04b18efee5b737730120c38c19f2e5d41a
Chris Warren <cwarren@williams.edu>
2013-03-26 09:59:31 -0400

    time blocks can load time block groups and reservations, reservations can load item, time block group, time blocks, and user; cmp functions for time blocks (by start time) and reservations (by order of item being reserved); test data for time blocks and reservations and comm prefs

--- classes/time_block.class.php
+++ classes/time_block.class.php
@@ -5,6 +5,34 @@ class TimeBlock extends Db_Linked
     public static $primaryKeyField = 'time_block_id';    
     public static $dbTable = 'time_blocks';
 
-    
-} 
+    public $time_block_group='';
+    public $user='';
+    public $reservations='';
+
+    public static function cmp($a,$b) {
+        if ($a->start_time == $b->start_time) {
+            return 0;
+        }
+        return ($a->start_time < $b->start_time) ? -1 : 1;
+    }
+
+    public function loadTimeBlockGroup() {
+        $this->time_block_group = TimeBlockGroup::getOneFromDb(['time_block_group_id'=>$this->time_block_group_id],$this->dbConnection);
+    }
+
+    public function loadUser() {
+        if (! $this->time_block_group) {
+            $this->loadTimeBlockGroup();
+        }
+        $this->user = User::getOneFromDb(['user_id'=>$this->time_block_group->user_id],$this->dbConnection);
+    }
+
+    public function loadReservations() {
+        if (! $this->time_block_group) {
+            $this->loadTimeBlockGroup();
+        }
+        $this->reservations = Reservation::getAllFromDb(['time_block_group_id'=>$this->time_block_group->time_block_group_id],$this->dbConnection);
+        usort($this->reservations,"Reservation::cmp");
+    }
+}
 ?>
---
+++ tests/app_infrastructure_tests/TestOfTimeBlock.class.php
@@ -0,0 +1,67 @@
+<?php
+	require_once dirname(__FILE__) . '/../simpletest/WMS_unit_tester_DB.php';
+
+	class TestOfTimeBlock extends WMSUnitTestCaseDB
+	{
+
+		function setUp() {
+            createAllTestData($this->DB);
+		}
+
+		function tearDown() {
+            removeAllTestData($this->DB);
+		}
+
+
+        //----------------
+        // static method tests
+
+        function testTimeBlockCmp() {
+            $t1 = new TimeBlock(['start_time'=>'2012-03-25 11:30:00','DB'=>$this->DB]);
+            $t2 = new TimeBlock(['start_time'=>'2012-03-25 10:30:00','DB'=>$this->DB]);
+            $t3 = new TimeBlock(['start_time'=>'2012-03-25 11:30:00','DB'=>$this->DB]);
+
+            $this->assertEqual(TimeBlock::cmp($t2,$t1),-1);
+            $this->assertEqual(TimeBlock::cmp($t1,$t3),0);
+            $this->assertEqual(TimeBlock::cmp($t1,$t2),1);
+        }
+
+        //----------------
+        // instance method tests
+
+        function testTimeBlockLoadTimeBlockGroup() {
+            $t = TimeBlock::getOneFromDb(['time_block_id'=>901],$this->DB);
+            $this->assertTrue($t->matchesDb);
+            $this->assertFalse($t->time_block_group);
+
+            $t->loadTimeBlockGroup();
+
+            $this->assertEqual($t->time_block_group->time_block_group_id,1001);
+        }
+
+        function testTimeBlockLoadUser() {
+            $t = TimeBlock::getOneFromDb(['time_block_id'=>901],$this->DB);
+            $this->assertTrue($t->matchesDb);
+            $this->assertFalse($t->user);
+
+            $t->loadUser();
+
+            $this->assertEqual($t->user->user_id,1101);
+        }
+
+        function testTimeBlockLoadReservations() {
+            $t = TimeBlock::getOneFromDb(['time_block_id'=>911],$this->DB);
+            $this->assertTrue($t->matchesDb);
+            $this->assertFalse($t->reservations);
+
+            $t->loadReservations();
+
+            $this->assertTrue(is_array($t->reservations));
+            $this->assertEqual(count($t->reservations),2);
+            $this->assertEqual($t->reservations[0]->reservation_id,809);
+            $this->assertEqual($t->reservations[1]->reservation_id,810);
+        }
+
+    }
+
+?>
