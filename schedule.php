b68d523561e1bf4385aed60b22d134921bc6bf53
Chris W <csw11235@gmail.com>
2015-09-30 13:10:06 -0400

    make schedules viewable by users other than admin and owner; schedules do not display reservations for deleted items

--- schedule.php
+++ schedule.php
@@ -3,9 +3,12 @@
 	require_once('head_pre_output.php');
 
 	$SCHED = Schedule::getOneFromDb(['schedule_id' => $_REQUEST['schedule']], $DB);
-	if ((!$USER->flag_is_system_admin) && ($SCHED->user_id != $USER->user_id)) {
-		util_redirectToAppHome('failure', 52);
-	}
+
+	$canEditSchedule = $USER->flag_is_system_admin || ($SCHED->user_id == $USER->user_id);
+
+//	if ((!$USER->flag_is_system_admin) && ($SCHED->user_id != $USER->user_id)) {
+//		util_redirectToAppHome('failure', 52);
+//	}
 
 	$SCHED->loadReservationsDeeply();
 
@@ -23,7 +26,8 @@
 	</div>
 	-->
 	<legend class="pull-left row-fluid">Schedule of Reservations
-		<a href="#" id="toggleEditMode" class="btn btn-medium btn-primary" data-cur-mode="view"><i class="icon-white icon-pencil"></i> Edit</a></legend>
+<?php if ($canEditSchedule) { ?>
+		<a href="#" id="toggleEditMode" class="btn btn-medium btn-primary" data-cur-mode="view"><i class="icon-white icon-pencil"></i> Edit</a><?php } ?></legend>
 
 	<div class="control-group">
 		<label class="control-label" for="reservations">Reservations on
@@ -32,7 +36,9 @@
 					$SCHED->loadTimeBlocks();
 					foreach ($SCHED->time_blocks as $tb) {
 						echo '<li id="list-of-time-block-' . $tb->time_block_id . '">';
-						echo '<a href="#" id="delete-time-block-' . $tb->time_block_id . '" class="editing-control hide btn btn-mini btn-danger btn-delete-list-item delete-time-block-btn" data-for-time-block="' . $tb->time_block_id . '"><i class="icon-trash icon-white"></i> </a> ';
+					        if ($canEditSchedule) {
+						     echo '<a href="#" id="delete-time-block-' . $tb->time_block_id . '" class="editing-control hide btn btn-mini btn-danger btn-delete-list-item delete-time-block-btn" data-for-time-block="' . $tb->time_block_id . '"><i class="icon-trash icon-white"></i> </a> ';
+						     }
 						echo $tb->toString() . "</li>\n";
 					}
 				?>
@@ -41,9 +47,11 @@
 
 		<p>
 			<small class="view-control"><span id="sched-notes-view"><?php echo $SCHED->notes; ?></span></small>
+<?php if ($canEditSchedule) { ?>
 		<div class="editing-control hide">
 			<textarea id="sched-notes" name="sched-notes" class="notes-editing-region"><?php echo $SCHED->notes; ?></textarea>
 		</div>
+<?php } ?>
 		</p>
 
 		<?php
@@ -54,6 +62,7 @@
 				echo '<p id="sched-is-manager-view" class="view-control text-info"><i class="icon-user"></i> <strong id="sched-is-manager-view-text">This is a regular user schedule</strong></p>';
 			}
 			if ($USER->canManageEqGroup($SCHED->reservations[0]->eq_item->eq_group->eq_group_id)) {
+			    if ($canEditSchedule) {
 				?>
 				<div class="editing-control hide text-warning">
 					<a href="#" id="sched-is-manager-btn" class="btn btn-medium <?php echo ($SCHED->type == 'manager') ? 'btn-warning' : 'btn-info'; ?>">
@@ -66,6 +75,7 @@
 					<br /><br />
 				</div>
 			<?php
+                            }
 			}
 		?>
 		<div class="controls">
@@ -76,9 +86,13 @@
 			<ul id="reservations">
 				<?php
 					foreach ($SCHED->reservations as $r) {
+					    if ($r->eq_item->name) {
 						echo '<li id="list-of-reservation-' . $r->reservation_id . '">';
+if ($canEditSchedule) {
 						echo '<a href="#" id="delete-reservation-' . $r->reservation_id . '" class="editing-control hide btn btn-mini btn-danger btn-delete-list-item delete-reservation-btn" data-for-reservation="' . $r->reservation_id . '"><i class="icon-trash icon-white"></i> </a> ';
+}
 						echo $r->eq_item->eq_subgroup->name . ': ' . $r->toString() . "</li>\n";
+					    }
 					}
 				?>
 			</ul>
@@ -86,9 +100,11 @@
 	</div>
 
 	<br /><br />
+<?php if ($canEditSchedule) { ?>
 	<a href="#" id="deleteEntireScheduleBtn" class="editing-control hide btn btn-medium btn-danger" data-for-schedule="<?php echo $SCHED->schedule_id; ?>"><i class="icon-trash icon-white"></i>
 		DELETE ENTIRE SCHEDULE AND RESERVATIONS</a>
 
 <?php
+}
 	require_once('foot.php');
 ?>
